86a3bc270b6b418716a593e5e1143a149f7036d0
Susie Stanley <susiestanley00@gmail.com>
2021-10-01 22:21:18 -0500

    updated spacing on details page

--- src/Home.js
+++ src/Home.js
@@ -12,33 +12,35 @@ export const Home = ({isAdmin}) => {
     return (
     <>
     <div className="home__flex">
+        <div className="home__flex--inner">
 
-        <div className="col1">
-            <div className="col1__inner">
+            <div className="col1 colspacer">
+                <div className="col1__inner">
 
-                <div className="headline">Cohort 51 Dog Grooming</div>
-                <div className="subtitle">Let us make your furbaby beautiful!</div>
+                    <div className="headline">Cohort 51 Dog Grooming</div>
+                    <div className="subtitle">Let us make your furbaby beautiful!</div>
 
-                <div className="welcome">
-                    <PropsAndState yourName={"Susie"} day={todaysDate} time={theTime}/>
+                    <div className="welcome">
+                        <PropsAndState yourName={"Susie"} day={todaysDate} time={theTime}/>
 
-                    {isAdmin ?
-                    <div>You are an Admin</div> 
-                    : <div>You are an not an Admin</div>}
+                        {isAdmin ?
+                        <div>You are an Admin</div> 
+                        : <div>You are an not an Admin</div>}
+
+                    </div>
+
+                    <div className="quote-list">
+                    <QuoteList />
+                    </div>
 
                 </div>
-
-                <div className="quote-list">
-                <QuoteList />
-                </div>
-
             </div>
-        </div>
 
-        <div className="col2">
-            <img className="col2__photo" src={require(`./images/homepagepic.png`).default} alt="cute dog with sign" />
-        </div>
+            <div className="col2 colspacer">
+                <img className="col2__photo" src={require(`./images/homepagepic.png`).default} alt="cute dog with sign" />
+            </div>
 
+        </div>
     </div>
     </>
     
