7d54f1c7e29e2cb80b5a2e5fb6f197ab317b91c2
Susie Stanley <susiestanley00@gmail.com>
2021-10-05 21:40:51 -0500

    added Animal Spotlight to home page

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,14 +1,25 @@
-import React from "react"
+import React, { useState } from "react"
 import { NavBar } from "./nav/NavBar"
 import { ApplicationViews } from "./ApplicationViews"
 import "./Kennel.css"
 
-const isAdmin = true;
+export const Kennel = () => {
+    const [isAuthenticated, setIsAuthenticated] = useState(sessionStorage.getItem("kennel_customer") !== null)
 
-export const Kennel = () => (
-    <>
-        <NavBar />
-        <ApplicationViews isAdmin = {isAdmin}/>
-    </>
-)
+    const setAuthUser = (user) => {
+        sessionStorage.setItem("kennel_customer", JSON.stringify(user))
+        setIsAuthenticated(sessionStorage.getItem("kennel_customer") !== null)
+    }
 
+    const clearUser = () => {
+        sessionStorage.clear();
+        setIsAuthenticated(sessionStorage.getItem("kennel_customer") !== null)
+      }
+
+    return (
+        <>
+            <NavBar clearUser={clearUser} isAuthenticated={isAuthenticated}/>
+            <ApplicationViews setAuthUser={setAuthUser} isAuthenticated={isAuthenticated}/>
+        </>
+    )
+}
